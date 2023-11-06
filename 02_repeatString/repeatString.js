9394c0f1596ffb90e0a6a86bd4046adc5720a028
psiservices-parkerschellenberg <parker.schellenberg@psionline.com>
2023-11-06 14:16:38 -0600

    02 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    if (times < 0) {
+        return "ERROR";
+    }
+    
+    let result = "";
+    for (let i = 1; i <= times; i++) {
+        result += string;
+    }
 
+    return result;
 };
 
 // Do not edit below this line
