3d015a3df572f4c921a8cd6a750d1b2646e1c703
psiservices-parkerschellenberg <parker.schellenberg@psionline.com>
2023-11-06 14:21:37 -0600

    03 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split("");
+    let reversedArray = array.reverse();
+    let reversedString = reversedArray.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
