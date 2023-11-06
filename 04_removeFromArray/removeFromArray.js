21b8f641b2bbf3faa061e8f00f58ed268eef9cd2
psiservices-parkerschellenberg <parker.schellenberg@psionline.com>
2023-11-06 14:41:34 -0600

    04 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(dasArray, ...args) {
+    args.forEach((arg) => {
+        const index = dasArray.indexOf(arg);
+        if (index > -1) {
+            dasArray.splice(index, 1);
+        }
+    });
 
+    return dasArray;
 };
 
 // Do not edit below this line
